5e928768dec248423dedc5ac6ede3076ac4c3bd0
loringfor <loringfor@outlook.com>
2017-05-16 20:46:42 +0000

    add hong

--- js/category.auto.js
+++ js/category.auto.js
@@ -1,12 +1,13 @@
 var defaultPageSize = 5;
 var arr = eval('[{"title":"初入linux世界","date":"2017-05-11 00:39:19","desc":"我只是一个刚刚学习linux的新手，把自己的学习经历以博客的形式记录下来"},\
 {"title":"编译机制","date":"2017-05-12 00:49:59","desc":"以自身学习经历来谈谈linux下源程序的编译过程"},\
-{"title":"make的使用","date":"2017-05-14 00:15:55","desc":"linux下，make是一个非常重要的工具"},\
+{"title":"make的使用","date":"2017-05-13 00:15:55","desc":"linux下，make是一个非常重要的工具"},\
 {"title":"git的学习","date":"2017-05-14 00:10:09","desc":"每一个程序员都必须熟练使用git"},\
 {"title":"Qt模块简介","date":"2017-05-14 00:39:52","desc":"对Qt5的模块进行一个简单的介绍，便于以后的需要去快速查找"},\
-{"title":"moc的作用","date":"2017-05-14 17:14:35","desc":"对于qt编译的一个补充，了解一下moc的作用"},\
+{"title":"moc的作用","date":"2017-05-15 17:14:35","desc":"对于qt编译的一个补充，了解一下moc的作用"},\
 {"title":"元对象","date":"2017-05-15 20:12:20","desc":"对于元对象（QMetaObject）的一个深入学习，剖析moc生成的文件"},\
-{"title":"QObject类详解","date":"2017-05-16 23:46:41","desc":"详细了解Qt中的基类QObject"}]')
+{"title":"QObject类详解","date":"2017-05-16 23:46:41","desc":"详细了解Qt中的基类QObject"},\
+{"title":"QObject宏","date":"2017-05-16 23:46:41","desc":"详细了解Qt中的基类QObject中的宏定义"}]')
 
 function get(currentPage) {
 	return getResult(currentPage, defaultPageSize);
