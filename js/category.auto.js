04af5f03c98d28e6bda26b4927141955d8a696b0
loringfor <loringfor@outlook.com>
2017-05-17 21:32:59 +0000

    add process and thread

--- js/category.auto.js
+++ js/category.auto.js
@@ -6,8 +6,12 @@ var arr = eval('[{"title":"初入linux世界","date":"2017-05-11 00:39:19","desc
 {"title":"Qt模块简介","date":"2017-05-14 00:39:52","desc":"对Qt5的模块进行一个简单的介绍，便于以后的需要去快速查找"},\
 {"title":"moc的作用","date":"2017-05-15 17:14:35","desc":"对于qt编译的一个补充，了解一下moc的作用"},\
 {"title":"元对象","date":"2017-05-15 20:12:20","desc":"对于元对象（QMetaObject）的一个深入学习，剖析moc生成的文件"},\
-{"title":"QObject类详解","date":"2017-05-16 23:46:41","desc":"详细了解Qt中的基类QObject"},\
-{"title":"QObject宏","date":"2017-05-16 23:46:41","desc":"详细了解Qt中的基类QObject中的宏定义"}]')
+{"title":"QObject类详解","date":"2017-05-15 23:46:41","desc":"详细了解Qt中的基类QObject"},\
+{"title":"QObject宏","date":"2017-05-16 09:46:41","desc":"详细了解Qt中的基类QObject中的宏定义"},\
+{"title":"进程的学习","date":"2017-05-16 20:00:59","desc":"对于进程的简单了解与学习"},\
+{"title":"进程间的通信","date":"2017-05-17 21:10:59","desc":"简单介绍了下进程间通信的几种机制"},\
+{"title":"线程的学习","date":"2017-05-17 23:59:11","desc":"对于线程的简单了解与学习"}\
+			   ]')
 
 function get(currentPage) {
 	return getResult(currentPage, defaultPageSize);
